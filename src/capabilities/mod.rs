9cb3b1cbc0b8e62c27f6520933fa84f5ab89dea7
Nonnetta <pengweihuang@we.cn>
2026-08-18 11:38:07 +0800

    fix: complete src migration and cleanup

--- src/capabilities/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod file_picker;
-pub mod notifications;
-pub mod opener;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    tracing_subscriber::fmt::init();
-
-    deepseek_desktop_lib::run()
-}
